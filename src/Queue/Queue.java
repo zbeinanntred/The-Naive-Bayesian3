65f20bfbbfbcfaa71f6bc4d7dca07f13a2e14094
zbeinanntred <hucangshan18286@163.com>
2021-05-10 01:43:51 +0800

    Add Queue package w placeholder logic
    Signed-off-by: Chris Emerson <chris.emerson@roledex.com>

---
+++ src/Queue/Queue.java
@@ -0,0 +1,19 @@
+package Queue;
+
+public class Queue<Item> {
+  public void enqueue(Item item) {
+
+  }
+
+  public Item dequeue() {
+    return null;
+  }
+
+  public boolean isEmpty() {
+    return true;
+  }
+
+  public int size() {
+    return 0;
+  }
+}
---
+++ src/Queue/QueueRunner.java
@@ -0,0 +1,8 @@
+package Queue;
+
+public class QueueRunner {
+  public static void main(String[] args) {
+    Queue queue = new Queue();
+    System.out.println("Created a queue!");
+  }
+}
