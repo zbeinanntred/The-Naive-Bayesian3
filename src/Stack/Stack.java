970095b612520ed17c619361b1979909297fd158
zbeinanntred <hucangshan18286@163.com>
2021-05-13 02:20:32 +0800

    Add Stack package w placeholder logic
    Signed-off-by: Chris Emerson <chris.emerson@roledex.com>

---
+++ src/Stack/Stack.java
@@ -0,0 +1,19 @@
+package Stack;
+
+public class Stack<Item> {
+  public void push() {
+
+  }
+
+  public Item pop() {
+    return null;
+  }
+
+  public boolean isEmpty() {
+    return true;
+  }
+
+  public int size() {
+    return 0;
+  }
+}
---
+++ src/Stack/StackRunner.java
@@ -0,0 +1,8 @@
+package Stack;
+
+public class StackRunner {
+  public static void main(String[] args) {
+    Stack stack = new Stack();
+    System.out.println("Created a stack!");
+  }
+}
