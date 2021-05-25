8f8fb808212dbe92c5c7c1f88cc14abdaa9661aa
zbeinanntred <hucangshan18286@163.com>
2021-05-26 04:10:54 +0800

    Create initial attempt at mergesort
    Signed-off-by: cemerson <cemerson@i3logix.com>

--- src/Runner.java
+++ src/Runner.java
@@ -1,11 +1,13 @@
 import Sorting.InsertionSort_Descending;
 import Sorting.InsertionSort_Initial;
 import Sorting.InsertionSort_Main;
+import Sorting.MergeSort_Initial;
 
 public class Runner {
   public static void main(String[] args) {
-    (new InsertionSort_Initial()).run();
-    (new InsertionSort_Main()).run();
-    (new InsertionSort_Descending()).run();
+//    (new InsertionSort_Initial()).run();
+//    (new InsertionSort_Main()).run();
+//    (new InsertionSort_Descending()).run();
+    (new MergeSort_Initial()).run();
   }
 }
---
+++ src/Sorting/MergeSort_Initial.java
@@ -0,0 +1,63 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class MergeSort_Initial extends SortingAlgorithm {
+    protected void sort(int[] arr) {
+        // we want to mutate the input but it's easier to return arrays in recursive sort
+        // so we'll just get an arr from mergeSort and then copy it over
+        int[] sortedArr = this.mergeSort(arr);
+
+        System.arraycopy(sortedArr, 0, arr, 0, arr.length);
+    }
+
+    private int[] mergeSort(int[] arr) {
+        // Arrays of length <=1 are already sorted
+        if (arr.length <= 1) return arr;
+
+        // split the array in two and recursively merge sort each half
+        int mid = arr.length / 2;
+        int[] sorted1 = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] sorted2 = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        // merge the two sorted halves
+        return merge(sorted1, sorted2);
+    }
+
+    private int[] merge(int[] sorted1, int[] sorted2) {
+        // if either array is empty, return the other, presumably nonempty one
+        if (sorted1.length == 0) {
+            return sorted2;
+        } else if (sorted2.length == 0) {
+            return sorted1;
+        }
+
+        int index1 = 0;
+        int index2 = 0;
+        int value1 = sorted1[0];
+        int value2 = sorted2[0];
+        int[] mergedArr = new int[sorted1.length + sorted2.length];
+
+        for (int i=0; i<mergedArr.length; i++) {
+            if (value1 < value2) {
+                mergedArr[i] = value1;
+                index1++;
+                if (index1 == sorted1.length) {
+                    value1 = Integer.MAX_VALUE;
+                } else {
+                    value1 = sorted1[index1];
+                }
+            } else {
+                mergedArr[i] = value2;
+                index2++;
+                if (index2 == sorted2.length) {
+                    value2 = Integer.MAX_VALUE;
+                } else {
+                    value2 = sorted2[index2];
+                }
+            }
+        }
+
+        return mergedArr;
+    }
+}
